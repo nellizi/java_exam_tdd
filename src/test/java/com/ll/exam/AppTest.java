937366f744650aacd2fcf93bcd70b4e4144cd112
nellizi <masterliz9095@gmail.com>
2022-07-05 16:33:02 +0900

    테스트케이스 생성

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -3,25 +3,21 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
-    public void 더하기() {
-        int rs = 5 + 5;
-        assertEquals(11, rs);
+    public void 더하기테스트(){
+        int rs = Calculator.add("10+20");
+        assertEquals(30,rs);
+    }
+    @Test
+    public void 더하기테스트_2(){
+        int rs = Calculator.add("20+20");
+        assertEquals(40,rs);
     }
 
-    @Test
-    public void 빼기(){
-        int mn = 7 - 2;
-        assertEquals(2,mn);
-    }
-
-    @Test
-    public void 곱하기(){
-        int x = 3 * 8;
-        assertEquals(24,x);
-    }
 }
