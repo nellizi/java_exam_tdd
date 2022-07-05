9ceec3c6395f869c72b3df2d7e4591f8b2dfc387
nellizi <masterliz9095@gmail.com>
2022-07-05 16:51:38 +0900

    ì덧셈 뺄셈 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -2,9 +2,17 @@ package com.ll.exam;
 
 public class Calculator {
     public static int add(String s) {
-        if (s.equals("10+20")) {
-            return 30;
-        }
-        return 40;
+        String[] sBits = s.split("\\+");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+        return no1 + no2 ;
+    }
+
+    public static int sub(String s) {
+        String[] sBits = s.split("\\-");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+        return no1 - no2 ;
+
     }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -19,5 +19,20 @@ public class AppTest {
         int rs = Calculator.add("20+20");
         assertEquals(40,rs);
     }
+    @Test
+    public void 더하기케이스_3(){
+        int rs = Calculator.add("5+20");
+        assertEquals(25,rs);
+    }
 
+    @Test
+    public void 빼기케이스(){
+        int mn = Calculator.sub("10-2");
+        assertEquals(8,mn);
+    }
+    @Test
+    public void 빼기케이스_2(){
+        int mn = Calculator.sub("5-10");
+        assertEquals(-5, mn);
+    }
 }
